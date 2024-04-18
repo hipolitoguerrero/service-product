6e3de3bd6934f2f5117e3688aa8c6d248903cf01
Hipólito Guerrero <hipolito.guerrero@gmail.com>
2024-04-17 22:12:21 -0400

    Versión inicial

---
+++ src/test/java/com/example/product/ProductRepositoryMockTest.java
@@ -0,0 +1,38 @@
+package com.example.product;
+
+import com.example.product.entity.Category;
+import com.example.product.entity.Product;
+import com.example.product.repository.ProductRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Date;
+import java.util.List;
+
+@DataJpaTest
+public class ProductRepositoryMockTest {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Test
+    public void whenFindByCategory_thenReturnListProduct(){
+        Product product01 = Product.builder()
+                .name("computer")
+                .category(Category.builder().id(1L).build())
+                .description("")
+                .stock(Double.parseDouble("10"))
+                .price(Double.parseDouble("630000.50"))
+                .status("Created")
+                .createAt(new Date()).build();
+
+        productRepository.save(product01);
+
+        List<Product> founds = productRepository.findByCategory(product01.getCategory());
+
+        Assertions.assertThat(founds.size()).isEqualTo(3);
+
+    }
+}
