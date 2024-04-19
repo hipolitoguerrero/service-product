6f170602808f6198f14a1c40ffcd2c0795253e22
Hipólito Guerrero <hipolito.guerrero@gmail.com>
2024-04-18 20:32:14 -0400

    REST Parte 2
    getProduct, createProduct, updateProduct, deleteProduct y updateStockProduct

--- README.md
+++ README.md
@@ -7,10 +7,28 @@ Tecnologías:
 - JPA / HIBERNATE
 
 Herramientas:
+- IntelliJ IDEA
 - Postman
 
 URLs:
 - GET http://localhost:8091/products
-
+- GET http://localhost:8091/products?categoryId=1
+- GET http://localhost:8091/products/1
+- POST http://localhost:8091/products
+/ JSON: {
+  "name": "Wallabee",
+  "description": "Comfort",
+  "stock": 1,
+  "price": 30,
+  "category": {"id":1,"name":"shoes"}
+  }
+- PUT http://localhost:8091/products/4 / JSON: {
+  "name": "Wallabee",
+  "description": "Comfort",
+  "stock": 1,
+  "price": 35,
+  "category": {"id":1,"name":"shoes"}
+  }
+- GET http://localhost:8091/products/3/stock?quantity=10
 
 From Digital Lab Academy
--- src/main/java/com/example/product/controller/ProductController.java
+++ src/main/java/com/example/product/controller/ProductController.java
@@ -1,12 +1,12 @@
 package com.example.product.controller;
 
+import com.example.product.entity.Category;
 import com.example.product.entity.Product;
 import com.example.product.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,11 +18,62 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping
-    public ResponseEntity<List<Product>> listProduct(){
-        List<Product> products = productService.listAllProduct();
-        if (products.isEmpty()){
-            return ResponseEntity.noContent().build();
+    public ResponseEntity<List<Product>> listProduct(@RequestParam(name = "categoryId", required = false) Long categoryId){
+        List<Product> products;
+        if(null == categoryId){
+            products = productService.listAllProduct();
+            if (products.isEmpty()){
+                return ResponseEntity.noContent().build();
+            }
+        }else{
+            products = productService.findByCategory(Category.builder().id(categoryId).build());
+            if (products.isEmpty()){
+                return ResponseEntity.notFound().build();
+            }
         }
         return ResponseEntity.ok(products);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable("id") Long id){
+        Product product = productService.getProduct(id);
+        if(null == product){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(product);
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product){
+        Product productCreate = productService.createProduct(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(productCreate);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable("id") Long id, @RequestBody Product product){
+        product.setId(id);
+        Product productDB = productService.updateProduct(product);
+        if(productDB == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(productDB);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Product> deleteProduct(@PathVariable("id") Long id){
+        Product productDelete = productService.deleteProduct(id);
+        if(productDelete == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(productDelete);
+    }
+
+    @GetMapping(value = "/{id}/stock")
+    public ResponseEntity<Product> updateStockProduct(@PathVariable Long id, @RequestParam(name = "quantity", required = true) Double quantity){
+        Product product = productService.updateStock(id, quantity);
+        if(product == null){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(product);
     }
 }
